66fdd44273c22a14b334ca74a8a8627323f04cbb
Brian Berns <brianberns@gmail.com>
2020-07-16 13:44:57 -0400

    More work on parser.

--- ConUom/Frink.fs
+++ ConUom/Frink.fs
@@ -3,6 +3,8 @@
 open System
 open System.IO
 
+open MathNet.Numerics
+
 open FParsec
 
 module private Parser =
@@ -39,31 +41,76 @@ module private Parser =
             skipPrefix
         ] |> many
 
+    let addUnit (name : string) (unit : Unit) =
+        updateUserState (Map.add name unit)
+
     let parseBaseUnit =
-        pipe5
+        pipe3
             identifier
-            spaces
-            (skipString "=!=")
-            spaces
+            (spaces >>. (skipString "=!=") .>> spaces)
             identifier
-            (fun dim _ _ _ name ->
-                Unit.createBase dim name)
+            (fun dim _ name ->
+                name, Unit.createBase dim name)
             |> attempt
 
-    let parse =
+    let consumeBaseUnit =
+        parse {
+            let! name, unit = parseBaseUnit
+            do! addUnit name unit
+        }
+
+    let parseBigInt =
+        many1Satisfy Char.IsDigit
+            |>> bigint.Parse
+
+    let parseBigRational =
+        parse {
+            let! num = parseBigInt
+            let! slashOpt = opt (skipChar '/')
+            if slashOpt.IsSome then
+                let! den = parseBigInt
+                return BigRational.FromBigIntFraction(num, den)
+            else
+                return BigRational.FromBigInt(num)
+        }
+
+    let parseDerivedUnit state =
+        pipe5
+            identifier
+            (spaces >>. (skipString ":=") .>> spaces)
+            (opt parseBigRational)
+            spaces
+            identifier
+            (fun newName _ scaleOpt _ oldName ->
+                let scale = scaleOpt |> Option.defaultValue 1N
+                let oldUnit : Unit = state |> Map.find oldName
+                let newUnit = scale @@ oldUnit
+                newName, newUnit)
+            |> attempt
+
+    let consumeDerivedUnit =
+        parse {
+            let! state = getUserState
+            let! name, unit = parseDerivedUnit state
+            do! addUnit name unit
+        }
+
+    let consumeUnit =
+        consumeBaseUnit <|> consumeDerivedUnit
+
+    let parseUnits =
         optional skipJunk
-            >>. sepBy parseBaseUnit skipJunk
+            >>. sepBy consumeUnit skipJunk
             .>> optional skipJunk
 
-    /// Runs the given parser on the given string.
-    let run parser str =
-        let parser = parser .>> eof   // force consumption of entire string
+    let parse str =
+        let parser = parseUnits .>> eof   // force consumption of entire string
         let str = if isNull str then "" else str
-        match runParserOnString parser () "" str with
-            | Success (result, _, _) -> result
+        match runParserOnString parser Map.empty "" str with
+            | Success (_, result, _) -> result
             | Failure (msg, _, _) -> failwith msg
 
 module Frink =
 
     let parseFile path =
-        File.ReadAllText path |> Parser.run Parser.parse
+        File.ReadAllText path |> Parser.parse
